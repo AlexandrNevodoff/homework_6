be2efd4262e2388d6258339b0dc5a09b4d5e02fa
Alexandr Nevodov <nevodoff@gmail.com>
2022-12-13 18:47:25 +0300

    init

---
+++ task_41/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// 41 task Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+for (int i = 0; i < n; i++)
+
+{
+    Console.Write("Введите число:");
+    if (Convert.ToInt32(Console.ReadLine()) > 0) counter=counter+1;
+}
+Console.WriteLine($"Чисел больше нуля: " +counter);
---
+++ task_43/Program.cs
@@ -0,0 +1,32 @@
+﻿//43 task Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// 43 task
+//Console.Clear();
+
+Console.Write("Введите k и b первой прямой: ");
+string[] numberOne = Console.ReadLine().Split(" ");
+double k1 = double.Parse(numberOne[0]);
+double b1 = double.Parse(numberOne[1]);
+Console.Write("Введите k и b второй прямой: ");
+string[] numberTwo = Console.ReadLine().Split(" ");
+double k2 = double.Parse(numberTwo[0]);
+double b2 = double.Parse(numberTwo[1]);
+
+// y = k1 * x + b1, y = k2 * x + b2
+// k1 * x + b1 = k2 * x + b2
+// (k1 - k2) * x = b2 - b1
+
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+
+double x = (b2 - b1) / (k1 - k2);
+//double y = k1 * x + b1;
+
+
+//Console.WriteLine((b2 - b1) / (k1 - k2));
+Console.WriteLine ($"Х равно: "+ x);
+double y = k2 * x + b1;
+//Console.WriteLine(k2 * ((b2 - b1) / (k1 - k2)) + b1);
+Console.WriteLine ($"Y Равен: "+ y);
